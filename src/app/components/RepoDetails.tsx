8d52ec61f19f76d76af9ba80cd6907109b705eab
kensylee <kingsleyumeze@yahoo.com>
2024-05-13 13:58:05 +0100

    update changes

--- src/app/components/RepoDetails.tsx
+++ src/app/components/RepoDetails.tsx
@@ -73,37 +73,39 @@ const RepoDetails = ({ repo }) => {
       <Divider />
       
       {/* Repository details */}
-      <Flex alignItems="center" justifyContent="space-between" mt={4} flexWrap="wrap">
-        <Flex alignItems="center" flexWrap="wrap">
+      <Flex flexDirection={{ base: "column", md: "row" }} alignItems="center" justifyContent="space-between" mt={4}>
+        <Flex alignItems="center" mb={{ base: 4, md: 0 }}>
           <Text mr={4} fontWeight="bold">
             Stars:
           </Text>
           <Text>{repo.stargazers_count}</Text>
-          <Text mx={4} fontWeight="bold">
+        </Flex>
+        <Flex alignItems="center" mb={{ base: 4, md: 0 }}>
+          <Text mr={4} fontWeight="bold">
             Forks:
           </Text>
           <Text>{repo.forks_count}</Text>
         </Flex>
-        <Flex alignItems="center" flexWrap="wrap" mt={{ base: 4, md: 0 }}>
-          <Text mx={4} fontWeight="bold">
+        <Flex alignItems="center" mb={{ base: 4, md: 0 }}>
+          <Text mr={4} fontWeight="bold">
             License:
           </Text>
           <Text>{repo.license ? repo.license.name : "Unknown"}</Text>
         </Flex>
-      </Flex>
-      <Flex alignItems="center" justifyContent="space-between" mt={4} flexWrap="wrap">
-        <Flex alignItems="center" flexWrap="wrap">
+        <Flex alignItems="center" mb={{ base: 4, md: 0 }}>
           <Text mr={4} fontWeight="bold">
             Language:
           </Text>
           <Text>{repo.language || "Unknown"}</Text>
-          <Text mx={4} fontWeight="bold">
+        </Flex>
+        <Flex alignItems="center">
+          <Text mr={4} fontWeight="bold">
             Created:
           </Text>
           <Text>{new Date(repo.created_at).toLocaleDateString()}</Text>
         </Flex>
-        <Flex alignItems="center" flexWrap="wrap" mt={{ base: 4, md: 0 }}>
-          <Text mx={4} fontWeight="bold">
+        <Flex alignItems="center">
+          <Text mr={4} fontWeight="bold">
             Last Updated:
           </Text>
           <Text>{new Date(repo.updated_at).toLocaleDateString()}</Text>
